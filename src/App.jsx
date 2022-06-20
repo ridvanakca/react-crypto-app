66df6f1f7ed66c8520f9fb1a9a8714fd329b25d1
Rıdvan Akca <akca.ridvann@gmail.com>
2022-06-20 16:11:16 +0300

    layout is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Router, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
-import Navbar from "./components/Navbar";
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from "./components";
 import "./App.css";
 
 const App = () => {
@@ -9,8 +9,31 @@ const App = () => {
       <div className='navbar'>
         <Navbar />
       </div>
-      <div className='main'></div>
-      <div className='footer'></div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route path='/' element={<Homepage />}></Route>
+              <Route path='/exchanges' element={<Exchanges />}></Route>
+              <Route path='/cryptocurrencies' element={<Cryptocurrencies />}></Route>
+              <Route path='/crypto/:coinId' element={<CryptoDetails />}></Route>
+              <Route path='/news' element={<News />}></Route>
+            </Routes>
+          </div>
+        </Layout>
+
+        <div className='footer'>
+          <Typography.Title level={5} style={{ color: "white", textAlign: "center" }}>
+            CryptoVerse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 };
